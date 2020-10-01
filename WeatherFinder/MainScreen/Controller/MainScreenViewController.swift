b064c717b51d054329804fea80ac1b2c1ec9ad8c
akushniarou <a.kushniarour@gmail.com>
2020-10-01 16:51:19 +0300

    Added background gradient

--- WeatherFinder/MainScreen/Controller/MainScreenViewController.swift
+++ WeatherFinder/MainScreen/Controller/MainScreenViewController.swift
@@ -2,17 +2,17 @@
 import UIKit
 
 class MainScreenViewController: UIViewController {
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     var searchController: UISearchController?
     
+    let colors = Colors()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSearchController()
-        
-        
-        
-        
+        refresh()
     }
     
     private func setupSearchController() {
@@ -23,11 +23,14 @@ class MainScreenViewController: UIViewController {
         searchController?.searchBar.placeholder = "Search for city"
     }
     
-    @IBAction func updateLocation(_ sender: UIBarButtonItem) {
-        
+    private func refresh() {
+        view.backgroundColor = .clear
+        let backgroundLayer = colors.gradientLayer!
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, at: 0)
     }
-    
-    
-    
+
+    @IBAction func updateLocation(_ sender: UIBarButtonItem) {
+    }
 }
 
--- WeatherFinder/MainScreen/View/GradientColor.swift
+++ WeatherFinder/MainScreen/View/GradientColor.swift
@@ -1,9 +1,15 @@
-//
-//  GradientColor.swift
-//  WeatherFinder
-//
-//  Created by Artur Kushniarou on 10/1/20.
-//  Copyright © 2020 Viktor Pelivan. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+class Colors {
+    var gradientLayer: CAGradientLayer!
+    
+    init() {
+        let colorTop = UIColor(red: 0.0 / 255.0, green: 0.0 / 255.0, blue: 250.0 / 255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 135.0 / 255.0, green: 206.0 / 255.0, blue: 250.0 / 255.0, alpha: 1.0).cgColor
+        
+        self.gradientLayer = CAGradientLayer()
+        self.gradientLayer.colors = [colorTop, colorBottom]
+        self.gradientLayer.locations = [0.0, 1.0]
+    }
+}
