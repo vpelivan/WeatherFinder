50a0ac0ea74afc8538b65fce3ef899051083686f
Viktor Pelivan <pelivan.v.1988@gmail.com>
2020-10-06 08:35:43 +0300

    added final modifier in ccell class, corrected the naming of function inside a class

--- WeatherFinder/MainScreen/View/Cells/CityWeatherTableViewCell.swift
+++ WeatherFinder/MainScreen/View/Cells/CityWeatherTableViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CityWeatherTableViewCell: UITableViewCell {
+final class CityWeatherTableViewCell: UITableViewCell {
 
     @IBOutlet private weak var cityLabel: UILabel!
     @IBOutlet private weak var temperatureLabel: UILabel!
@@ -24,7 +24,7 @@ class CityWeatherTableViewCell: UITableViewCell {
     //TODO: declare NetworkManager class example, to reach "getWeatherImage(iconId: String, completion: @escaping (UIImage) -> ())" method (task: https://trello.com/c/fKkoSAWB)
     
     //TODO: current method must be implemented in task: https://trello.com/c/4Wjbg7Ph
-    public func fetchWeatherData(/* model: CityWeather */) {
+    func updateWeatherData(/* model: CityWeather */) {
         /* inside this method, depending on current model data, all
          appropriate model values must be assigned in UI Elemets values;
          current method should call
