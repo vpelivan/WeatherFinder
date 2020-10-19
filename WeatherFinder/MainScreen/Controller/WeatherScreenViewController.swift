208eb16c4d5ef77841bea3a5e64fb1c4196065ec
a.kushniarou@gmail.com <a.kushniarou@gmail.com>
2020-10-19 18:06:56 +0300

    Customized TableView in main screen controller

--- WeatherFinder/MainScreen/Controller/WeatherScreenViewController.swift
+++ WeatherFinder/MainScreen/Controller/WeatherScreenViewController.swift
@@ -8,18 +8,22 @@ class WeatherScreenViewController: UIViewController {
     
     // TODO: resolve variable searchController initialization issue
     private var searchController: UISearchController?
-    
     private let gradientLayer = Colors.gradientLayer
+    private var cityWeatherData: WeatherDataModel? {
+        // TODO: cityWeatherData must be computable and should call networkManager corresponding methods or geolocation methods.
+        return nil
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isTranslucent = false
         setupSearchController()
         refresh()
+        setupTableView()
     }
     
     override public func traitCollectionDidChange(_ previouseTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previouseTraitCollection)
-        
         gradientLayer.frame = view.bounds
     }
     
@@ -37,6 +41,26 @@ class WeatherScreenViewController: UIViewController {
         view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
+    private func setupTableView() {
+        let nib = UINib(nibName: "CityWeatherTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "weatherCell")
+        tableView.dataSource = self
+    }
+    
     @IBAction private func updateLocation(_ sender: UIBarButtonItem) {
+    }
+}
+
+extension WeatherScreenViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! CityWeatherTableViewCell
+        if let weatherData = cityWeatherData{
+            cell.updateWeatherData(model: weatherData)
+        }
+        return cell
     }
 }
