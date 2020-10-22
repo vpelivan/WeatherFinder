318bc6ca71d1d403562ffc2b1615e613c922a5de
 TempVolodymyr <nativeassasin1988@gmail.com>
2020-10-23 00:27:04 +0300

    Add 2 functions for getting image: 1) use network service to take image(private) 2) use first function giving it info about kind of icon change signature of getWeatherImage

--- WeatherFinder/MainScreen/NetworkLayer/Manager/NetworkManager.swift
+++ WeatherFinder/MainScreen/NetworkLayer/Manager/NetworkManager.swift
@@ -11,7 +11,7 @@ import UIKit
 
 protocol NetManagerProtocol {
     func getWeatherDataByCityName(name: String, expression: @escaping (WeatherDataModel?, Error?) -> ())
-    func getWeatherImage()
+    func getWeatherImage(iconID: String, expression: @escaping (UIImage?, Error?) -> Void)
     func getWeatherByCoordinates(longitude: Double, latitude: Double)
 }
 
@@ -60,12 +60,61 @@ class NetworkManager: NetManagerProtocol {
         })
     }
     
-    
-    func getWeatherImage()
+    func getWeatherImage(iconID: String, expression: @escaping (UIImage?, Error?) -> Void) //data.hashValue method
     {
-        //TODO
+        switch iconID {
+            case "01d": imageRequest(type: ImagesEndpoint.clearSkyDay, expression: {(image, error) -> Void in
+                do {
+                        try expression(image,nil)
+                }
+                catch {}
+            })
+//            case "01n": ImagesEndpoint.clearSkyNight
+//            case "02d": ImagesEndpoint.fewCloudsDay
+//            case "02n": ImagesEndpoint.fewCloudsNight
+//            case "03d": ImagesEndpoint.scatteredCloudsDay
+//            case "03n": ImagesEndpoint.scatteredCloudsNight
+//            case "04d": ImagesEndpoint.brokenClouds
+//            case "04n": ImagesEndpoint.brokenCloudsNight
+//            case "09d": ImagesEndpoint.showerRainDay
+//            case "09n": ImagesEndpoint.showerRainNight
+//            case "10d": ImagesEndpoint.rainDay
+//            case "10n": ImagesEndpoint.rainNight
+//            case "11d": ImagesEndpoint.thunderstormDay
+//            case "11n": ImagesEndpoint.thunderstormNight
+//            case "13d": ImagesEndpoint.snowDay
+//            case "13n": ImagesEndpoint.snowNight
+//            case "50d": ImagesEndpoint.mist
+//            case "50n": ImagesEndpoint.mistNight
+//        TODO other cases of image
+        }
     }
     
+    private func imageRequest(type: EndPointType, expression: @escaping (UIImage?, Error?) -> Void) {
+        networkSevice.makeRequest(with: type, cachePolicy: .useProtocolCachePolicy, completion: {(dataImage, response, error) -> Void in
+            do {
+                let httpResponse = response as? HTTPURLResponse
+                switch httpResponse?.statusCode {
+                    case 200 :
+                        guard let imgData = dataImage else {
+                            throw NetworkError.notFound
+                        }
+                        let image = UIImage(data: imgData)
+                        expression(image, nil)
+                        
+                    case 404: throw NetworkError.notFound
+                        
+                    case .none: expression(nil, error)
+                        
+                    case .some(_): expression(nil, nil) //TODO make logic for other status codes
+                }
+            }
+            catch let error
+            {
+                expression(nil, error)
+            }
+        })
+    }
     
     func getWeatherByCoordinates(longitude: Double, latitude: Double) {
         <#code#>//TODO
